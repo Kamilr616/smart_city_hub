6cbfb3c045f37efe92f3fa108a98a6e1908de620
kamilr616 <kamilr616@gmail.com>
2024-03-16 16:44:16 +0100

    website title

--- source/web/smart_city_hub Web App/vite.config.ts
+++ source/web/smart_city_hub Web App/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'smart_city_hub'
+  base: '/smart_city_hub/'
 })
