121c76124d813d25d2e7d691ca24e5919a04a615
kamilr616 <kamilr616@gmail.com>
2024-05-15 20:07:00 +0200

    authentication

--- source/server/api/lib/middlewares/auth.middleware.ts
+++ source/server/api/lib/middlewares/auth.middleware.ts
@@ -1,27 +1,28 @@
-import { Request, Response, NextFunction } from 'express';
+import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
-import { config } from '../config';
-import { IUser } from "../modules/models/user.model";
+import {config} from '../config';
+import {IUser} from "../modules/models/user.model";
 
 export const auth = (request: Request, response: Response, next: NextFunction) => {
-   let token = request.headers['x-access-token'] || request.headers['authorization'];
-   if (token && typeof token === 'string') {
-       if (token.startsWith('Bearer ')) {
-           token = token.slice(7, token.length);
-       }
-       try {
-           jwt.verify(token, config.JwtSecret, (err, decoded) => {
-               if (err) {
-                   return response.status(400).send('Invalid token.');
-               }
-               //const user: IUser = decoded as IUser;
-               next();
-               return;
-           });
-       } catch (ex) {
-           return response.status(400).send('Invalid token.');
-       }
-   } else {
-       return response.status(401).send('Access denied. No token provided.');
-   }
+    let token = request.headers['x-access-token'] || request.headers['authorization'];
+    if (token && typeof token === 'string') {
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
+        }
+        try {
+            jwt.verify(token, config.JwtSecret, (err, decoded) => {
+                if (err) {
+                    return response.status(400).send('Invalid token.');
+                }
+                const user: IUser = decoded as IUser;
+                response.locals.userRole = user.role;
+                next();
+                return;
+            });
+        } catch (ex) {
+            return response.status(400).send('Invalid token.');
+        }
+    } else {
+        return response.status(401).send('Access denied. No token provided.');
+    }
 };
--- source/server/api/lib/middlewares/deviceIdParam.middleware.ts
+++ source/server/api/lib/middlewares/deviceIdParam.middleware.ts
@@ -1,11 +1,11 @@
-import { RequestHandler, Request, Response, NextFunction } from 'express';
-import { config } from "../config";
+import {RequestHandler, Request, Response, NextFunction} from 'express';
+import {config} from "../config";
 
 export const checkIdParam: RequestHandler = (request: Request, response: Response, next: NextFunction) => {
-   const { id } = request.params;
-   const parsedValue = parseInt(id, 10);
-   if (isNaN(2) || parsedValue >= config.supportedDevicesNum) {
-       return response.status(400).send('Bk lub niepoprawny parametr ID urządzenia!');
-   }
-   next();
+    const {id} = request.params;
+    const parsedValue = parseInt(id, 10);
+    if (isNaN(parsedValue) || parsedValue >= config.supportedDevicesNum) {
+        return response.status(400).send('Błąd lub niepoprawny parametr ID urządzenia!');
+    }
+    next();
 };
