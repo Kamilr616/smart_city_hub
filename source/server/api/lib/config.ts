a4a0ae1fc8440ea82c1270bc16f460b1f09c2416
kamilr616 <kamilr616@gmail.com>
2024-06-18 13:14:01 +0200

    project directories clean-up

--- source/server/api/lib/config.ts
+++ source/server/api/lib/config.ts
@@ -1,8 +1,25 @@
-export const config = {
-    port: process.env.PORT || 4200,
+require('dotenv').config()
+
+interface Config {
+    port: string;
+    supportedDevicesNum: number;
+    supportedSensorsNum: number;
+    JwtSecret: string;
+    databaseUrl: string;
+}
+
+const getEnvVariable = (key: string): string => {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environment variable ${key} is not set.`);
+    }
+    return value;
+}
+
+export const config: Config = {
+    port: getEnvVariable('PORT'),
     supportedDevicesNum: 96,
     supportedSensorsNum: 2,
-    JwtSecret: 'REDACTED', //secret
-    databaseUrl: process.env.MONGODB_URI || 'REDACTED'
+    JwtSecret: getEnvVariable('JWT_SECRET_KEY'),
+    databaseUrl: getEnvVariable('MONGODB_URI')
 };
-
