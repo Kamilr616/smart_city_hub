414dcc09a3cb9bc8443bf885ffb1c1c084c9ce4d
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-06-19 14:44:51 +0200

    tt

--- source/web/react/src/components/AdminSidebar.jsx
+++ source/web/react/src/components/AdminSidebar.jsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import kiLogo from '../assets/ki_LOGO_b.svg'
 
+
 function AdminSidebar({ setSelectedTab }) {
   const { user, setUser, logout } = useContext(UserContext);
   const navigate = useNavigate();
@@ -19,6 +20,7 @@ function AdminSidebar({ setSelectedTab }) {
     <>
       <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-full w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
         <div className="mb-2 p-4">
+          <img src={kiLogo} alt="KI" className="mr-2"/>
           <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">
             Smart City Hub
           </h5>
@@ -36,7 +38,8 @@ function AdminSidebar({ setSelectedTab }) {
             role="button"
             tabIndex="0"
             className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-            onClick={() => setSelectedTab(`HomeLights`)}
+            onClick={() => setSelectedTab(`controlPanel`)}
+
           >
             <div className="grid place-items-center mr-4">
               <svg
@@ -51,6 +54,7 @@ function AdminSidebar({ setSelectedTab }) {
             </div>
             Home Lights
           </div>
+
 
           <div
             role="button"
